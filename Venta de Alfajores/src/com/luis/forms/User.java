21c2379e6cd278b498651851d2ea219624456757
Luis Carranza <luiscarranza3395@gmail.com>
2020-10-25 23:20:15 -0500

    Clasr user

---
+++ Venta de Alfajores/src/com/luis/forms/User.java
@@ -0,0 +1,38 @@
+package com.luis.forms;
+
+public class User {
+
+	private int id;
+	private String username;
+	private String email;
+	private String website;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getWebsite() {
+		return website;
+	}
+	public void setWebsite(String website) {
+		this.website = website;
+	}
+	
+	
+	
+}
